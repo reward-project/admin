724d5c829f9d6142a532ae901ff8d3a07b86e624
윤여원 <dudnjsckrgo@gmail.com>
2024-11-26 23:58:25 +0900

    dashboard 리다이렉트

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,21 @@
 // /login/page.tsx
-
+"use client"
 import { CategoryChannelsComponent } from "@/components/category-channels";
-import React from "react";
+import useUserStore from "@/store/useUserStore";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 export default function AdminLoginPage() {
+      const { userInfo,  hasHydrated } = useUserStore(
+        (state) => state
+      );
+      const router = useRouter();
+      useEffect(() => {
+        if (hasHydrated && userInfo) {
+          // hasHydrated가 true이고 userInfo가 없는 경우에만 동작
+          router.push("/dashboard");
+        }
+      }, [userInfo, router, hasHydrated]);
+   
   return <></>;
 }
